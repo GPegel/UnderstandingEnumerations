e5a13d6cb55c84667f12d64ccf3bc80d80897d76
Gerhard Pegel <gpegel@gmail.com>
2015-09-13 23:04:31 +0200

    My first C# console application about Understanding Enumerations

---
+++ UnderstandingEnumerations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingEnumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            // Console.WriteLine ("Hello World!");
+
+            Console.WriteLine("Type a superhero's name to see his nickname:");
+            string userValue = Console.ReadLine();
+
+            Superhero myValue;
+
+            if (Enum.TryParse<Superhero>(userValue, true, out myValue))
+            {
+
+                switch (myValue)
+                {
+                    case Superhero.Batman:
+                        Console.WriteLine("Caped Cruisader");
+                        break;
+                    case Superhero.Spiderman:
+                        Console.WriteLine("Sticky guy");
+                        break;
+                    case Superhero.Superman:
+                        Console.WriteLine("Man of steel");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    enum Superhero
+    {
+        Batman,
+        Spiderman,
+        Superman
+    }
+}
